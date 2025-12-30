cde8e4c88f62d3d097dff16aaff0dbbae55bf575
Kevin Padilla <contact@jistro.xyz>
2025-12-29 18:52:38 -0600

    feat: remove unused imports and constants in SongDBScript; streamline contract setup

--- script/SongDataBase.s.sol
+++ script/SongDataBase.s.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {SongDB} from "@shine/contracts/SongDB/SongDB.sol";
 
 contract SongDBScript is Script {
-    address public constant ADMIN = 0x5cBf2D4Bbf834912Ad0bD59980355b57695e8309;
-    SongDB public songDataBase;
+
 
     function setUp() public {}
 
