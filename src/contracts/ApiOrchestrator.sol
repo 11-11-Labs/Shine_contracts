2769c8e3abe40d9981eafcb0bd45631c908dac1e
Kevin Padilla <contact@jistro.xyz>
2026-01-09 15:28:41 -0600

    feat: implement ApiOrchestrator contract for managing song, album, and user functionalities by api

---
+++ src/contracts/ApiOrchestrator.sol
@@ -0,0 +1,402 @@
+// SPDX-License-Identifier: SHINE-PPL-1.0
+pragma solidity ^0.8.20;
+
+/**
+    ___ _ _____  _____ シ
+  ,' _//// / / |/ / _/ ャ
+ _\ `./ ` / / || / _/  イ
+/___,/_n_/_/_/|_/___/  ヌ
+                      
+                                                            
+ * @title Shine ISongDB
+ * @author 11:11 Labs 
+ * @notice This contract manages song metadata, user purchases, 
+ *         and admin functionalities for the Shine platform.
+ */
+
+import {ISongDB} from "@shine/interface/ISongDB.sol";
+import {IAlbumDB} from "@shine/interface/IAlbumDB.sol";
+import {IArtistDB} from "@shine/interface/IArtistDB.sol";
+import {IUserDB} from "@shine/interface/IUserDB.sol";
+import {OwnableRoles} from "@solady/auth/OwnableRoles.sol";
+
+contract ApiOrchestrator is OwnableRoles {
+    struct DataBaseList {
+        address album;
+        address artist;
+        address song;
+        address user;
+    }
+
+    uint256 constant ADMIN_ROLE = 1;
+    uint256 constant API_ROLE = 2;
+
+    DataBaseList private dbAddress;
+
+    bytes16 breakerAddressSetup;
+    bytes16 breakerShop;
+
+    event SongPurchased(
+        uint256 indexed songId,
+        uint256 indexed userId,
+        uint256 price
+    );
+
+    event AlbumPurchased(
+        uint256 indexed albumId,
+        uint256 indexed userId,
+        uint256 price
+    );
+
+    constructor(
+        address initialOwner,
+        address initialAdminAddress,
+        address initialAPIAddress
+    ) {
+        _initializeOwner(initialOwner);
+        _grantRoles(initialAdminAddress, ADMIN_ROLE);
+        _grantRoles(initialAPIAddress, API_ROLE);
+    }
+
+    function _setDatabaseAddresses(
+        address _dbalbum,
+        address _dbartist,
+        address _dbsong,
+        address _dbuser
+    ) external onlyOwner {
+        if (breakerAddressSetup != bytes16(0)) revert();
+        dbAddress.album = _dbalbum;
+        dbAddress.artist = _dbartist;
+        dbAddress.song = _dbsong;
+        dbAddress.user = _dbuser;
+        breakerAddressSetup = bytes16(hex"00000000000000000000000000000001");
+    }
+
+    function registerArtist(
+        string memory name,
+        string memory metadataURI,
+        address artistAddress
+    ) external onlyRoles(API_ROLE) returns (uint256 id) {
+        id = IArtistDB(dbAddress.artist).register(name, metadataURI, artistAddress);
+    }
+
+    function chnageDataOfArtist(
+        uint256 artistId,
+        string memory name,
+        string memory metadataURI,
+        address artistAddress
+    ) external onlyRoles(API_ROLE) {
+        if (!IArtistDB(dbAddress.artist).exists(artistId)) revert();
+        IArtistDB(dbAddress.artist).changeBasicData(artistId, name, metadataURI);
+
+        if (IArtistDB(dbAddress.artist).getAddress(artistId) != artistAddress) {
+            IArtistDB(dbAddress.artist).changeAddress(artistId, artistAddress);
+        }
+    }
+
+    function registerUser(
+        string memory username,
+        string memory metadataURI,
+        address userAddress
+    ) external onlyRoles(API_ROLE) returns (uint256) {
+        return IUserDB(dbAddress.user).register(username, metadataURI, userAddress);
+    }
+
+    function changeDataOfUser(
+        uint256 userId,
+        string memory username,
+        string memory metadataURI,
+        address userAddress
+    ) external onlyRoles(API_ROLE) {
+        if (!IUserDB(dbAddress.user).exists(userId)) revert();
+        IUserDB(dbAddress.user).changeBasicData(userId, username, metadataURI);
+
+        if (IUserDB(dbAddress.user).getAddress(userId) != userAddress) {
+            IUserDB(dbAddress.user).changeAddress(userId, userAddress);
+        }
+    }
+
+    function registerSong(
+        uint256 artistId,
+        string memory title,
+        uint256[] memory artistIDs,
+        string memory mediaURI,
+        string memory metadataURI,
+        bool canBePurchased,
+        uint256 price
+    ) external onlyRoles(API_ROLE) returns (uint256) {
+        if (!IArtistDB(dbAddress.artist).exists(artistId)) revert();
+
+        return
+            ISongDB(dbAddress.song).register(
+                title,
+                artistId,
+                artistIDs,
+                mediaURI,
+                metadataURI,
+                canBePurchased,
+                price
+            );
+    }
+
+    function changeDataOfSong(
+        uint256 songId,
+        string memory title,
+        uint256 principalArtistId,
+        uint256[] memory artistIDs,
+        string memory mediaURI,
+        string memory metadataURI,
+        bool canBePurchased,
+        uint256 price
+    ) external onlyRoles(API_ROLE) {
+        if (!ISongDB(dbAddress.song).exists(songId)) revert();
+        if (
+            ISongDB(dbAddress.song).getPrincipalArtistId(songId) !=
+            principalArtistId
+        ) revert();
+
+        ISongDB(dbAddress.song).change(
+            songId,
+            title,
+            principalArtistId,
+            artistIDs,
+            mediaURI,
+            metadataURI,
+            canBePurchased,
+            price
+        );
+    }
+
+    function changePurchaseabilityAndPriceOfSong(
+        uint256 songId,
+        uint256 artistId,
+        bool canBePurchased,
+        uint256 price
+    ) external onlyRoles(API_ROLE) {
+        if (!ISongDB(dbAddress.song).exists(songId)) revert();
+        if (ISongDB(dbAddress.song).getPrincipalArtistId(songId) != artistId) revert();
+
+        ISongDB(dbAddress.song).changePurchaseability(songId, canBePurchased);
+        ISongDB(dbAddress.song).changePrice(songId, price);
+    }
+
+    function registerAlbum(
+        uint256 artistId,
+        string memory title,
+        string memory metadataURI,
+        uint256[] memory songIDs,
+        uint256 price,
+        bool canBePurchased,
+        bool isASpecialEdition,
+        string memory specialEditionName,
+        uint256 maxSupplySpecialEdition
+    ) external onlyRoles(API_ROLE) returns (uint256) {
+        if (!IArtistDB(dbAddress.artist).exists(artistId)) revert();
+        for (uint256 i = 0; i < songIDs.length; i++) {
+            if (!ISongDB(dbAddress.song).exists(songIDs[i])) revert();
+        }
+
+        return
+            IAlbumDB(dbAddress.album).register(
+                title,
+                artistId,
+                metadataURI,
+                songIDs,
+                price,
+                canBePurchased,
+                isASpecialEdition,
+                specialEditionName,
+                maxSupplySpecialEdition
+            );
+    }
+
+    function changeDataOfAlbum(
+        uint256 albumId,
+        string memory title,
+        uint256 principalArtistId,
+        string memory metadataURI,
+        uint256[] memory songIDs,
+        uint256 price,
+        bool canBePurchased,
+        bool isASpecialEdition,
+        string memory specialEditionName,
+        uint256 maxSupplySpecialEdition
+    ) external onlyRoles(API_ROLE) {
+        if (!IAlbumDB(dbAddress.album).exists(albumId)) revert();
+
+        if (
+            IAlbumDB(dbAddress.album).getPrincipalArtistId(principalArtistId) !=
+            principalArtistId
+        ) revert();
+
+        for (uint256 i = 0; i < songIDs.length; i++) {
+            if (!ISongDB(dbAddress.song).exists(songIDs[i])) revert();
+        }
+
+        IAlbumDB(dbAddress.album).change(
+            albumId,
+            title,
+            principalArtistId,
+            metadataURI,
+            songIDs,
+            price,
+            canBePurchased,
+            isASpecialEdition,
+            specialEditionName,
+            maxSupplySpecialEdition
+        );
+    }
+
+    function changePurchaseabilityAndPriceOfAlbum(
+        uint256 albumId,
+        uint256 artistId,
+        bool canBePurchased,
+        uint256 price
+    ) external onlyRoles(API_ROLE) {
+        if (!IAlbumDB(dbAddress.album).exists(albumId)) revert();
+        if (IAlbumDB(dbAddress.album).getPrincipalArtistId(albumId) != artistId)
+            revert();
+
+        IAlbumDB(dbAddress.album).changePurchaseability(albumId, canBePurchased);
+        IAlbumDB(dbAddress.album).changePrice(albumId, price);
+    }
+
+    function buySong(
+        uint256 songId,
+        uint256 userId
+    ) external onlyRoles(API_ROLE) {
+        if (!ISongDB(dbAddress.song).exists(songId)) revert();
+        if (!IUserDB(dbAddress.user).exists(userId)) revert();
+        if (ISongDB(dbAddress.song).hasUserPurchased(songId, userId)) revert();
+        if (ISongDB(dbAddress.song).canUserBuy(songId, userId)) revert();
+
+        uint256 price = ISongDB(dbAddress.song).getPrice(songId);
+        if (IUserDB(dbAddress.user).getBalance(userId) < price) revert();
+
+        uint256 principalArtistId = ISongDB(dbAddress.song).getPrincipalArtistId(
+            songId
+        );
+
+        IUserDB(dbAddress.user).deductBalance(userId, price);
+        IArtistDB(dbAddress.artist).addBalance(principalArtistId, price);
+        ISongDB(dbAddress.song).purchase(songId, userId);
+        IUserDB(dbAddress.user).addSong(userId, songId);
+
+        emit SongPurchased(songId, userId, price);
+    }
+
+    function buyAlbum(
+        uint256 albumId,
+        uint256 userId
+    ) external onlyRoles(API_ROLE) {
+        if (!IAlbumDB(dbAddress.album).exists(albumId)) revert();
+        if (!IUserDB(dbAddress.user).exists(userId)) revert();
+        if (IAlbumDB(dbAddress.album).hasUserPurchased(albumId, userId)) revert();
+        if (IAlbumDB(dbAddress.album).canUserBuy(albumId, userId)) revert();
+
+        uint256 price = IAlbumDB(dbAddress.album).getPrice(albumId);
+        if (IUserDB(dbAddress.user).getBalance(userId) < price) revert();
+
+        uint256 principalArtistId = IAlbumDB(dbAddress.album).getPrincipalArtistId(
+            albumId
+        );
+
+        IUserDB(dbAddress.user).deductBalance(userId, price);
+        IArtistDB(dbAddress.artist).addBalance(principalArtistId, price);
+        uint256[] memory songIds = IAlbumDB(dbAddress.album).purchase(albumId, userId);
+
+        IUserDB(dbAddress.user).addSongs(userId, songIds);
+
+        emit AlbumPurchased(albumId, userId, price);
+    }
+
+    function addBalanceToUser(
+        uint256 userId,
+        uint256 amount
+    ) external onlyRoles(API_ROLE) {
+        if (!IUserDB(dbAddress.user).exists(userId)) revert();
+        IUserDB(dbAddress.user).addBalance(userId, amount);
+    }
+
+    function deductBalanceFromUser(
+        uint256 userId,
+        uint256 amount
+    ) external onlyRoles(API_ROLE) {
+        if (!IUserDB(dbAddress.user).exists(userId)) revert();
+        IUserDB(dbAddress.user).deductBalance(userId, amount);
+    }
+
+    function addBalanceToArtist(
+        uint256 artistId,
+        uint256 amount
+    ) external onlyRoles(API_ROLE) {
+        if (!IArtistDB(dbAddress.artist).exists(artistId)) revert();
+        IArtistDB(dbAddress.artist).addBalance(artistId, amount);
+    }
+
+    function deductBalanceFromArtist(
+        uint256 artistId,
+        uint256 amount
+    ) external onlyRoles(API_ROLE) {
+        if (!IArtistDB(dbAddress.artist).exists(artistId)) revert();
+        IArtistDB(dbAddress.artist).deductBalance(artistId, amount);
+    }
+
+    function setAPIRole(
+        address apiAddress
+    ) external onlyRolesOrOwner(ADMIN_ROLE) {
+        _grantRoles(apiAddress, API_ROLE);
+    }
+
+    function revokeApiRole(
+        address apiAddress
+    ) external onlyRolesOrOwner(ADMIN_ROLE) {
+        _removeRoles(apiAddress, API_ROLE);
+    }
+
+    function setAdminRole(address adminAddress) external onlyOwner {
+        _grantRoles(adminAddress, ADMIN_ROLE);
+    }
+
+    function revokeAdminRole(address adminAddress) external onlyOwner {
+        _removeRoles(adminAddress, ADMIN_ROLE);
+    }
+
+    function migrateOrchestrator(
+        address newOrchestratorAddress
+    ) external onlyOwner {
+        if (newOrchestratorAddress == address(0)) revert();
+        IAlbumDB(dbAddress.album).transferOwnership(newOrchestratorAddress);
+        IArtistDB(dbAddress.artist).transferOwnership(newOrchestratorAddress);
+        ISongDB(dbAddress.song).transferOwnership(newOrchestratorAddress);
+        IUserDB(dbAddress.user).transferOwnership(newOrchestratorAddress);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+You like snooping around code, don't you?
+⠄⠄⢸⠃⠄⠛⠉⠄⣤⣤⣤⣤⣤⣄⠉⠙⠻⣿⣿⣿⣿⡇⣶⡄⢢⢻⣿⣿⣮⡛
+⠄⠄⠘⢀⣠⣾⠄⠘⠋⠉⠉⠛⠻⢿⣦⡲⣄⠈⠻⣿⣇⣣⠹⣯⣄⣦⡙⢿⣿⣿
+⢀⡎⠄⣾⠋⠄⠄⠄⣠⣤⣤⣤⣤⣄⠈⠙⣍⢳⠄⠘⣿⡐⠁⢉⣁⣀⡀⠄⠙⠿
+⡼⠄⡆⡇⢀⣤⡆⣿⣿⣿⣿⣿⣿⣿⣷⡘⣿⣷⣄⡀⢹⡇⣾⣿⣿⣿⣿⡇⣄⢠
+⡇⠄⢿⡔⢿⣿⡇⢿⣿⣿⣿⠿⠿⢿⣿⠇⣿⣿⣿⠇⢈⣁⡻⢿⣿⣀⠈⣱⢏⣾
+⢣⠄⢆⢩⣮⣿⣿⣄⠻⣿⣷⣤⣤⡴⢋⣴⣿⣿⡟⠄⢸⣿⣿⣷⣶⣶⠾⢫⣪⠅
+⠘⣆⠈⢑⣘⣿⣿⣿⣷⣶⣤⣤⣴⣾⣿⣿⣿⣿⠃⢀⣿⣿⣿⣿⣿⣿⣿⣿⢇⣴
+⠄⠘⢦⡈⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣀⣉⣉⣹⣿⣿⣿⣿⠿⡃⠪⣶
+⠄⠄⠄⠙⠢⢄⡈⠛⠻⠿⠿⠿⠟⠛⠋⣀⠰⣿⣿⣿⣿⡿⠿⡛⠉⡄⠄⠄⠄⣀
+⠄⠄⠄⠄⢀⡾⢉⣁⠄⠄⠄⠲⠂⢂⠋⠄⠛⠒⠉⠉⠑⠒⠉⠒⠒⡧⠤⠖⢋⣡
+ */
