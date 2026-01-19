cc5835a3e32276f6bc8b8b23fda3ef46254c8985
Kevin Padilla <contact@jistro.xyz>
2026-01-18 19:41:11 -0600

    feat: add fuzz and unit revert tests for orchestrator user and artist data management

---
+++ test/uint/revert/orchestrator/Orchestrator_test_unit_revert_UserArtist.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Constants} from "../../../Constants.sol";
+import {ArtistDB} from "@shine/contracts/database/ArtistDB.sol";
+import {UserDB} from "@shine/contracts/database/UserDB.sol";
+import {ErrorsLib} from "@shine/contracts/orchestrator/library/ErrorsLib.sol";
+
+contract Orchestrator_test_unit_revert_UserArtist is Constants {
+    function test_unit_revert_chnageBasicData_AddressIsNotOwnerOfArtistId()
+        public
+    {
+        uint256 artistId = _execute_orchestrator_register(
+            true,
+            "initial_artist",
+            "https://arweave.net/initialURI",
+            ARTIST_1.Address
+        );
+
+        vm.startPrank(WILDCARD_ACCOUNT.Address);
+        vm.expectRevert(ErrorsLib.AddressIsNotOwnerOfArtistId.selector);
+        orchestrator.chnageBasicData(
+            true,
+            artistId,
+            "updated_artist",
+            "https://arweave.net/updatedURI"
+        );
+        vm.stopPrank();
+    }
+
+    function test_unit_revert_chnageBasicData_AddressIsNotOwnerOfUserId()
+        public
+    {
+        uint256 userId = _execute_orchestrator_register(
+            false,
+            "initial_user",
+            "https://arweave.net/initialUserURI",
+            USER.Address
+        );
+
+        vm.startPrank(WILDCARD_ACCOUNT.Address);
+        vm.expectRevert(ErrorsLib.AddressIsNotOwnerOfUserId.selector);
+        orchestrator.chnageBasicData(
+            false,
+            userId,
+            "updated_user",
+            "https://arweave.net/updatedUserURI"
+        );
+        vm.stopPrank();
+    }
+
+    function test_unit_revert_changeAddress_AddressIsNotOwnerOfArtistId()
+        public
+    {
+        uint256 artistId = _execute_orchestrator_register(
+            true,
+            "artist_name",
+            "https://arweave.net/artistURI",
+            ARTIST_1.Address
+        );
+
+        vm.startPrank(WILDCARD_ACCOUNT.Address);
+        vm.expectRevert(ErrorsLib.AddressIsNotOwnerOfArtistId.selector);
+        orchestrator.changeAddress(true, artistId, WILDCARD_ACCOUNT.Address);
+        vm.stopPrank();
+    }
+
+    function test_unit_revert_changeAddress_AddressIsNotOwnerOfUserId() public {
+        uint256 userId = _execute_orchestrator_register(
+            false,
+            "user_name",
+            "https://arweave.net/userURI",
+            USER.Address
+        );
+
+        vm.startPrank(WILDCARD_ACCOUNT.Address);
+        vm.expectRevert(ErrorsLib.AddressIsNotOwnerOfUserId.selector);
+        orchestrator.changeAddress(false, userId, WILDCARD_ACCOUNT.Address);
+        vm.stopPrank();
+    }
+}
